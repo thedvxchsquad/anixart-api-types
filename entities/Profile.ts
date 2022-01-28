01b0599b62894c4ffddc57a4a2ebff9e898f56f0
whyrinki <naer200469@gmail.com>
2022-01-28 17:12:17 +0300

    update entities

--- entities/Profile.ts
+++ entities/Profile.ts
@@ -1,4 +1,7 @@
-import { Release } from "./Release" 
+import { ProfileToken } from "./ProfileToken";
+import { ProfileWatchDynamics } from "./ProfileWatchDynamics";
+import { Release } from "./Release";
+import { Role } from "./Role";
 
 export class Profile {
 
@@ -30,5 +33,44 @@ export class Profile {
     is_blocked: boolean;
     is_comment_notifications_enabled: boolean;
     is_counts_hidden: boolean;
+    is_episode_notifications_enabled: boolean;
+    is_first_episode_notification_enabled: boolean;
+    is_friend_requests_disallowed: boolean;
+    is_google_bound: boolean;
+    is_login_changed: boolean;
+    is_me_blocked: boolean;
+    is_online: boolean;
+    is_perm_banned: boolean;
+    is_social: boolean;
+    
+    is_social_hidden: boolean;
+    is_sponsor: boolean;
+    is_stats_hidden: boolean;
+    is_verified: boolean;
+    is_vk_bound: boolean;
+
+    last_activity_time: number;
+    plan_count: number;
+    privilege_level: number;
+    profileToken: ProfileToken;
+    rating_score: number;
+    register_date: number;
+    sponsorshipExpires: number;
+    vote: number;
+    votes: Release[];
+    watchingCount: number;
+    
+
+    login: string = "";
+    password: string = "";
+    avatar: string = "";
+    status: string = "";
+    vk_page: string = "";
+    tg_page: string = "";
+    inst_page: string = "";
+    tt_page: string = "";
+
+    watch_dynamics: ProfileWatchDynamics[] = [];
+    roles: Role[] = [];
 
 }
