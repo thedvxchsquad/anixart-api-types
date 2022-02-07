529a60ccbaa14717f8b22f264e99060cd91199d2
Erik Kiznov <erikfaaazlyev@gmail.com>
2022-02-07 19:37:48 +0500

    class -> interface

--- entities/Profile.ts
+++ entities/Profile.ts
@@ -3,71 +3,92 @@ import { ProfileWatchDynamics } from "./ProfileWatchDynamics";
 import { Release } from "./Release";
 import { Role } from "./Role";
 
-interface APIProfileInterface {}
+enum ProfileTypes {
+  NONE, MEMBER, RELEASER, MODERATOR, ADMINISTRATOR, DEVELOPER
+};
 
-class Profile {
-  public static ADMINISTRATOR = 4;
-  public static DEVELOPER = 5;
-  public static MEMBER = 1;
-  public static MODERATOR = 3;
-  public static NONE = 0;
-  public static RELEASER = 2;
+interface Profile {
+  ban_expires: number;
+  ban_reason: string | null;
+  added_date: number; // blockListAddedDate
+  collection_count: number;
+  comment_count: number;
+  completed_count: number;
+  dropped_count: number;
+  favorite_count: number;
+  friend_count: number;
+  friend_status: number | null;
+  history: Release[];
+  hold_on_count: number;
 
-  public ban_expires: number;
-  public ban_reason: string | null;
-  public added_date: number; // blockListAddedDate
-  public collection_count: number;
-  public comment_count: number;
-  public completed_count: number;
-  public dropped_count: number;
-  public favorite_count: number;
-  public friend_count: number;
-  public friend_status: number | null;
-  public history: Release[];
-  public hold_on_count: number;
+  id: number;
+  is_banned: boolean;
+  is_blocked: boolean;
+  is_comment_notifications_enabled: boolean;
+  is_counts_hidden: boolean;
+  is_episode_notifications_enabled: boolean;
+  is_first_episode_notification_enabled: boolean;
+  is_friend_requests_disallowed: boolean;
+  is_google_bound: boolean;
+  is_login_changed: boolean;
+  is_me_blocked: boolean;
+  is_online: boolean;
+  is_perm_banned: boolean;
+  is_social: boolean;
+  is_social_hidden: boolean;
+  is_sponsor: boolean;
+  is_stats_hidden: boolean;
+  is_verified: boolean;
+  is_vk_bound: boolean;
+  last_activity_time: number;
+  plan_count: number;
+  privilege_level: number;
+  profileToken: ProfileToken;
+  rating_score: number;
+  register_date: number;
+  sponsorshipExpires: number;
+  vote: number;
+  votes: Release[];
+  watching_count: number;
+  /**
+   * @default ""
+   */
+  login: string;
+  /**
+   * @default ""
+   */  
+  password: string;
+  /**
+   * @default ""
+   */
+  avatar: string;
+  /**
+   * @default ""
+   */
+  status: string;
+  /**
+   * @default ""
+   */
+  vk_page: string;
+  /**
+   * @default ""
+   */
+  tg_page: string;
+  /**
+   * @default ""
+   */
+  inst_page: string;
+  /**
+   * @default ""
+   */
+  tt_page: string;
+  /**
+   * @default []
+   */  
+  watch_dynamics: ProfileWatchDynamics[];
 
-  public id: number;
-  public is_banned: boolean;
-  public is_blocked: boolean;
-  public is_comment_notifications_enabled: boolean;
-  public is_counts_hidden: boolean;
-  public is_episode_notifications_enabled: boolean;
-  public is_first_episode_notification_enabled: boolean;
-  public is_friend_requests_disallowed: boolean;
-  public is_google_bound: boolean;
-  public is_login_changed: boolean;
-  public is_me_blocked: boolean;
-  public is_online: boolean;
-  public is_perm_banned: boolean;
-  public is_social: boolean;
-  public is_social_hidden: boolean;
-  public is_sponsor: boolean;
-  public is_stats_hidden: boolean;
-  public is_verified: boolean;
-  public is_vk_bound: boolean;
-  public last_activity_time: number;
-  public plan_count: number;
-  public privilege_level: number;
-  public profileToken: ProfileToken;
-  public rating_score: number;
-  public register_date: number;
-  public sponsorshipExpires: number;
-  public vote: number;
-  public votes: Release[];
-  public watching_count: number;
-  public login = "";
-  public password = "";
-  public avatar = "";
-  public status = "";
-  public vk_page = "";
-  public tg_page = "";
-  public inst_page = "";
-  public tt_page = "";
-  public watch_dynamics: ProfileWatchDynamics[] = [];
-
-  public roles: Role[] = [];
-
-  setProfile(raw: any) {
-    Object.assign(this, raw);
-  }
+  /**
+   * @default []
+   */  
+  roles: Role[];
 }
