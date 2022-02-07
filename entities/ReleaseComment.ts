4afa196b4c314804a551042affaa1bc6dcc28a93
Erik Kiznov <erikfaaazlyev@gmail.com>
2022-02-07 20:49:47 +0500

    add interface

--- entities/ReleaseComment.ts
+++ entities/ReleaseComment.ts
@@ -1,3 +1,24 @@
-export interface ReleaseComment {
+import { Profile } from "./Profile";
+import { Release } from "./Release";
 
+enum Votes {
+  NONE, MINUS, PLUS
+}
+
+export interface ReleaseComment {
+  id: number;
+  is_deleted: boolean;
+  is_edited: boolean;
+  is_spoiler: boolean;
+  message: string;
+  /**
+   * @default 0
+   */
+  parent_comment_id: number | null;
+  profile: Profile;
+  release: Release;
+  reply_count: number;
+  timestamp: number;
+  vote: number;
+  vote_count: number;
 }
